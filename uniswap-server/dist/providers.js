628a08f41b35a605ece754c050624b595b08e67a
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2024-09-06 09:47:16 -0400

    stop committing .js files

--- uniswap-server/dist/providers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getProvider = getProvider;
-const ethers_1 = require("ethers");
-const config_1 = require("./config");
-function getProvider() {
-    return new ethers_1.JsonRpcProvider(config_1.MainnetConfig.rpc.mainnet);
-}
